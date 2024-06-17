0c1e56d903452adcf31f812612ce64c34683e0be
Yuri Moreira Leocadio <yurileocadio0411@gmail.com>
2024-06-17 18:43:20 -0300

    first commit

---
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -0,0 +1,13 @@
+package com.poo.introducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoApplication.class, args);
+	}
+
+}
